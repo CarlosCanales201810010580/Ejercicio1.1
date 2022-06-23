03d5bc169a64e035fc7ef36dbb852732c0da069c
CarlosCanales201810010580 <79998068+CarlosCanales201810010580@users.noreply.github.com>
2022-06-23 11:20:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/operaciones/Funciones.java
@@ -0,0 +1,52 @@
+package com.example.operaciones;
+
+public class Funciones
+{
+    private Integer num1;
+    private Integer num2;
+    private Integer suma;
+    private Integer resta;
+    private Integer multiplicacion;
+    private Integer division;
+
+    public Funciones(int i, int parseInt){}//constructor vacio
+
+    public Funciones(Integer a, Integer b, Integer suma, Integer resta, Integer multiplicacion, Integer division){
+        this.num1 = a;
+        this.num1 = b;
+    }
+
+    //get y set
+    public Integer getNum1(){
+        return num1;
+    }
+
+    public void setNum1(Integer num1){
+        this.num1 = num1;
+    }
+
+    public Integer getNum2(){
+        return num2;
+    }
+
+    public void setNum2(Integer num2){
+        this.num2 = num2;
+    }
+
+    //calculos
+    public Integer suma (Integer a, Integer b){
+        return suma = a + b;
+    }
+
+    public Integer resta (Integer a, Integer b){
+        return resta = a - b;
+    }
+
+    public Integer multiplicacion (Integer a, Integer b){
+        return multiplicacion = a * b;
+    }
+
+    public Integer division (Integer a, Integer b){
+        return division = a / b;
+    }
+}// FuncionesMatematicas
